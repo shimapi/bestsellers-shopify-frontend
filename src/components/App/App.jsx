44991b97c358d192f01b1566212c6ae98dfdfa0d
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-02-27 15:16:14 -0300

    feat: Header OK

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,14 @@
+import Main from "../Main/Main";
 import Nav from "../Nav/Nav";
 import "./App.scss";
-// import ThemeSwitch from "../ThemeSwitch/ThemeSwitch.jsx";
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch.jsx";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<Main />
+			<ThemeSwitch />
 		</div>
 	);
 }
