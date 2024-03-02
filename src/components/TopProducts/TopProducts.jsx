61efa7bbd93048e6c2f07cf75350ee92510cbf1e
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-03-01 22:33:34 -0300

    feat: adding top 10

--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -1,10 +1,29 @@
 import Title from "../Title/Title";
+import "./TopProducts.scss";
+import { products } from "../../utils/data.js";
+
+const sortingProducts = products.sort((a, b) => b.sales - a.sales);
 
 const TopProducts = () => {
 	return (
 		<>
 			<section className="top-products">
 				<Title sentence="Top 10" />
+				<div className="top-products__container">
+					{sortingProducts.slice(0, 10).map((product, index) => {
+						console.log(product);
+						return (
+							<article className="top-products__product" key={index}>
+								<img
+									className="top-products__product-image"
+									src={product.image}
+									alt={product.name}
+								/>
+								<h4 className="top-products__product-name">{product.name}</h4>
+							</article>
+						);
+					})}
+				</div>
 			</section>
 		</>
 	);
