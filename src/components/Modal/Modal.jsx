f8ab4da43154588b2c7e3af7f955db82793ab62c
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-03-17 00:26:08 -0300

    feat: working on gallery modal photos in products

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,18 @@
-//import PathConstants from "@/routes/pathConstants";
 import { useEffect, useState } from "react";
 import "./Modal.scss";
+// Swiper
+import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Keyboard, Pagination, Navigation } from "swiper/modules";
+register();
+// end of Swiper
 
-const Modal = ({ image, handleCloseModal }) => {
+const Modal = ({ image, handleCloseModal, product, handleImgID }) => {
 	console.log("image", image);
+	console.log("handleImgID", handleImgID);
 	const [isModalOpen, setIsModalOpen] = useState("modal-closed");
 
 	useEffect(() => {
@@ -12,14 +21,41 @@ const Modal = ({ image, handleCloseModal }) => {
 		}
 	}, [image]);
 
-	console.log("modal image", image);
 	return (
 		<div className={`modal ${isModalOpen}`}>
 			<span className="modal__close" onClick={handleCloseModal}>
 				&#x2715;
 			</span>
 			<div className="modal__container">
-				<img src={image} alt="modal" className="modal__image" />
+				<Swiper
+					key={handleImgID}
+					slidesPerView={1}
+					effect={"fade"}
+					keyboard={{
+						enabled: true,
+					}}
+					pagination={{
+						clickable: true,
+					}}
+					navigation={true}
+					initialSlide={handleImgID}
+					modules={[Keyboard, Pagination, Navigation]}
+					className="mySwiper"
+				>
+					{product.data.images.length > 0 &&
+						product.data.images.map((image, index) => {
+							console.log("handleImgID desde map en modal", handleImgID);
+							return (
+								<SwiperSlide className="product__image-container" key={index}>
+									<img
+										src={image.src}
+										alt={product.data.title}
+										className="modal__image"
+									/>
+								</SwiperSlide>
+							);
+						})}
+				</Swiper>
 			</div>
 		</div>
 	);
